885652be4106e24fa6cafdd8de93d84f7a34adf9
tomas-stejskal <tomas.stejskal@akademiasovy.sk>
2016-05-13 11:14:06 +0200

    AutoInsertPlane.cs v0.0.0.2

---
+++ RezervaciaMiesteniek/AutoInsertPlane.cs
@@ -0,0 +1,60 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RezervaciaMiesteniek
+{
+    class AutoInsertPlane
+    {
+        MySqlConnection connection;
+        DateTime systemDate = DateTime.Now;
+
+        public AutoInsertPlane()
+        {
+            string connectionStr = "SERVER=localhost;DATABASE=plane;UID=root;PASSWORD=1234;";
+            connection = new MySqlConnection(connectionStr);
+        }
+
+        private bool OpenCon()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                return false;
+            }
+        }
+
+
+
+
+        /****************************************************************/
+        public void autoInset()
+        {
+            systemDate = systemDate.AddDays(30);
+            systemDate = systemDate.AddDays(-8).AddHours(5);
+            Console.WriteLine(systemDate.ToString());
+        }
+    }
+}
---
+++ RezervaciaMiesteniek/Loading.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RezervaciaMiesteniek
+{
+    public partial class Loading : Form
+    {
+        public Loading()
+        {
+            InitializeComponent();
+        }
+
+        private void Loading_Shown(object sender, EventArgs e)
+        {
+            AutoInsertPlane insertPlane = new AutoInsertPlane();
+            insertPlane.autoInset();
+            this.Dispose();
+        }
+    }
+}
