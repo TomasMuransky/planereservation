9eb7f4de336e5aeb18612b0b335dca0f63d93fd2
TomasMuransky <muransky.tomas@gmail.com>
2016-05-18 10:50:44 +0200

    uprava designu

--- RezervaciaMiesteniek/LoginWindow.Designer.cs
+++ RezervaciaMiesteniek/LoginWindow.Designer.cs
@@ -59,7 +59,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(291, 303);
+            this.button1.Location = new System.Drawing.Point(288, 303);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(105, 23);
             this.button1.TabIndex = 4;
@@ -69,9 +69,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(402, 303);
+            this.button2.Location = new System.Drawing.Point(399, 303);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(99, 23);
+            this.button2.Size = new System.Drawing.Size(105, 23);
             this.button2.TabIndex = 5;
             this.button2.Text = "New Account";
             this.button2.UseVisualStyleBackColor = true;
