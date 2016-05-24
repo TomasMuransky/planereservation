e3048a15f19869d5a9f29504370041db9fc4a3bb
MiliadisMoumtzoglou <miltiadis.moumtzoglou@akademiasovy.sk>
2016-05-24 15:03:55 +0200

    Miltiadis database

---
+++ RezervaciaMiesteniek/Loading.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RezervaciaMiesteniek
+{
+    public partial class Loading : Form
+    {
+        
+        private void LoadigThread()
+        {
+            AutoInsertPlane insertPlane = new AutoInsertPlane();
+            insertPlane.autoInset();
+            //this.Dispose();
+        }
+
+        public Loading()
+        {
+            InitializeComponent();
+        }
+
+        private void Loading_Shown(object sender, EventArgs e)
+        {
+            Thread th = new Thread(LoadigThread); //nove vlakno
+            //th.Start();
+            AutoInsertPlane insertPlane = new AutoInsertPlane();
+            insertPlane.autoInset();
+            this.Dispose();
+        }
+    }
+}
