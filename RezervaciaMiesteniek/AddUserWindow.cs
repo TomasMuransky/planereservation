efdc8477a9e49f1bdb478968b4f8d973da09f6e2
osvaldmarian <marian.osvald@akademiasovy.sk>
2016-05-24 15:07:38 +0200

    Marian commit

---
+++ RezervaciaMiesteniek/AddUserWindow.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RezervaciaMiesteniek
+{
+    public partial class AddUserWindow : Form
+    {
+        public AddUserWindow()
+        {
+            InitializeComponent();
+            textBox2.PasswordChar = '\u2022';
+            textBox3.PasswordChar = '\u2022';
+            /***************************/
+            label5.Text = "";
+            label6.Text = "";
+            label7.Text = "";
+            label8.Text = "";
+            label12.Text = "";
+            label13.Text = "";
+            label14.Text = "";
+            /***************************/
+        }
+
+
+
+        private bool phoneReg(string Phone) //regex phone verification
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(Phone))
+                    return false;
+                var r = new Regex(@"^\(?([0-9]{3})\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})$");
+                return r.IsMatch(Phone);
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool mailReg(string emailaddress) //e-mail verification taky mensy hack :) 
+        {
+            if (emailaddress.Equals(""))
+                return false;
+            try
+            {
+                MailAddress m = new MailAddress(emailaddress);
+
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string login = textBox1.Text;
+            string password = textBox2.Text;
+            string password_werification = textBox3.Text;
+            string email = textBox4.Text;
+            string phone = textBox5.Text;
+            string first_name = textBox6.Text;
+            string last_name = textBox7.Text;
+            /***************************************/
+            label5.Text = "";
+            label6.Text = "";
+            label7.Text = "";
+            label8.Text = "";
+            label12.Text = "";
+            label13.Text = "";
+            label14.Text = "";
+            /**************************************/
+
+            if (!login.Equals("") && !password.Equals("") && password.Equals(password_werification) &&
+                mailReg(email) && phoneReg(phone) && !first_name.Equals("") && !last_name.Equals(""))
+            {
+               
+                DataBase data = new DataBase();
+                if (data.isLoginUnique(login)) //oveerenie jedinecnosti loginu
+                {
+                    data.addNewRegistreation(login, password, email, phone, first_name, last_name);  //registravia noveho klienta
+                    this.Dispose();
+                }
+                else
+                {
+                    label5.Text = "login is not unique";
+                }
+
+               
+
+               
+            }
+            else //tu budu funkcie vypisujuce chybove hlasky
+            {
+                if (login.Equals(""))
+                    label5.Text = "login is required";
+                if (!password.Equals(password_werification))
+                    label7.Text = "passwor verification failed";
+                if (password.Equals(""))
+                    label6.Text = "password is required";
+                if (!phoneReg(phone))
+                    label12.Text = "bad phone format";
+                if (!mailReg(email))
+                    label8.Text = "bad e-mail format";
+                if (first_name.Equals(""))
+                    label13.Text = "first name is require";
+                if (last_name.Equals(""))
+                    label14.Text = "last name is required";
+               
+            }
+            
+        }
+    }
+}
---
+++ RezervaciaMiesteniek/AutoInsertPlane.cs
@@ -0,0 +1,268 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RezervaciaMiesteniek
+{
+    class AutoInsertPlane
+    {
+        MySqlConnection connection;
+        DateTime systemDate = DateTime.Now;
+
+        public AutoInsertPlane()
+        {
+            string connectionStr = "";
+            try
+            {
+                connectionStr = System.IO.File.ReadAllText("sqlConnectivity.conf");
+            }
+            catch
+            {
+                System.Windows.Forms.MessageBox.Show("file: sqlConnectivity.conf not found!");
+                System.Windows.Forms.Application.Exit();
+            }
+            connection = new MySqlConnection(connectionStr);
+        }
+
+        private bool OpenCon() //ovorenie spojenia
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+
+        private bool CloseConnection() //uzavretie pripojeniea
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                return false;
+            }
+        }
+        /***************************************************************/
+        /*                                                             */
+        /*                                                             */
+        /*                                                             */
+        /*                                                             */
+        /***************************************************************/
+        private bool checkIsPaneExist(DateTime future)
+        {
+            string result;
+
+            string sqlQuery = "select * from plane_destinations where TimeOfDeparture like '"+future.ToString("yyyy-MM-dd")+"%';"; //dotaz
+            MySqlCommand cmd = new MySqlCommand(sqlQuery, connection);
+            if (OpenCon())
+            {
+                try
+                {
+                    MySqlDataReader reader = cmd.ExecuteReader();
+                    reader.Read();
+                    result = reader["id"].ToString();
+                    CloseConnection();
+                    if (result != null)
+                        return true;
+                    else
+                        return false;
+                }
+                catch (MySqlException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            
+            if (connection != null) 
+                CloseConnection();
+            return false;
+        }
+
+        private void addPlanne() //nove lietadlo
+        {
+           
+            if (OpenCon())
+            {
+                string query = "insert into planes(planeName) values('sovy_air_lines');";
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                catch (MySqlException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                CloseConnection();
+            }
+        }
+        private string getPlaneID() //zisanie id posledneho lietadla
+        {
+            string res = null;
+            if (OpenCon())
+            {
+                string query = "select max(id) as id from planes;";
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                try
+                {
+                    MySqlDataReader reader = cmd.ExecuteReader();
+                    reader.Read();
+                    res = reader["id"].ToString();
+                }
+                catch(MySqlException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                CloseConnection();
+            }
+            return res;
+        }
+
+        private void addSeats(string plane_ID) //pridane sedadiel 4x vlolane
+        {
+            for (int i = 1; i <= 80; i++)
+            {
+                if (OpenCon())
+                {
+                    string query = "insert into seats(planeId,seatId,passengerId,seatIsTaken) values('" + plane_ID + "','" + i.ToString() + "',0,'N');";
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (MySqlException e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                    CloseConnection();
+                }
+            }
+        }
+        /***************/
+        private void addPlaneDestination1(DateTime date, string plane_id) //pridanie prvej cesty
+        {
+            if (OpenCon())
+            {
+                string query = "INSERT INTO Plane_Destinations(planeId,TimeOfDeparture,TimeOfArriving,fromId,toId) VALUES('" + plane_id+"','"+date.ToString("yyyy-MM-dd")+ " 07:00:00','"+ date.ToString("yyyy-MM-dd") + " 08:00:00','Kosice','Bratislava');";
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                catch(MySqlException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                CloseConnection();
+                addSeats(plane_id);
+            }
+        }
+        /***************/
+        private void addPlaneDestination2(DateTime date, string plane_id) //pridanie druhej cesty
+        {
+            if (OpenCon())
+            {
+                string query = "INSERT INTO Plane_Destinations(planeId,TimeOfDeparture,TimeOfArriving,fromId,toId) VALUES('" + plane_id + "','" + date.ToString("yyyy-MM-dd") + " 07:00:00','" + date.ToString("yyyy-MM-dd") + " 08:00:00','Bratislava','Kosice');";
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                catch (MySqlException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                CloseConnection();
+                addSeats(plane_id);
+            }
+        }
+        /***************/
+        private void addPlaneDestination3(DateTime date, string plane_id) //pridanie tretej cesty
+        {
+            if (OpenCon())
+            {
+                string query = "INSERT INTO Plane_Destinations(planeId,TimeOfDeparture,TimeOfArriving,fromId,toId) VALUES('" + plane_id + "','" + date.ToString("yyyy-MM-dd") + " 19:00:00','" + date.ToString("yyyy-MM-dd") + " 20:00:00','Kosice','Bratislava');";
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                catch (MySqlException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                CloseConnection();
+                addSeats(plane_id);
+            }
+        }
+        /***************/
+        private void addPlaneDestination4(DateTime date, string plane_id) //pridanie stvrtej cesty
+        {
+            if (OpenCon())
+            {
+                string query = "INSERT INTO Plane_Destinations(planeId,TimeOfDeparture,TimeOfArriving,fromId,toId) VALUES('" + plane_id + "','" + date.ToString("yyyy-MM-dd") + " 19:00:00','" + date.ToString("yyyy-MM-dd") + " 20:00:00','Bratislava','Kosice');";
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                catch (MySqlException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                CloseConnection();
+                addSeats(plane_id);
+            }
+        }
+
+        private void addNewPlane(DateTime date) //seria prikazov na pridanie do databazy
+        {
+            string plane_id;
+            addPlanne(); //prva cesta
+            plane_id = getPlaneID();
+            addPlaneDestination1(date, plane_id);
+            addPlanne(); //druha cesta
+            plane_id = getPlaneID();
+            addPlaneDestination2(date, plane_id);
+            addPlanne(); //treria cesta
+            plane_id = getPlaneID();
+            addPlaneDestination3(date, plane_id);
+            addPlanne(); //stvrta cesta
+            plane_id = getPlaneID();
+            addPlaneDestination4(date, plane_id);
+        }
+
+
+        /****************************************************************/
+        public void autoInset() //jedina public metoda
+        {
+            DateTime future = systemDate.AddDays(30); //datum 30 dni do predu
+            Console.WriteLine(getPlaneID());
+
+            for (int i =0; i < 30; i++)
+            {
+                if (!checkIsPaneExist(future))
+                {
+                    addNewPlane(future);
+                    future = future.AddDays(-1);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
